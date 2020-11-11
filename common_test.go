9029b4c99bfe2c0e021c26fdab965c65bdc3d83f
vt128 <jiduduluo@gmail.com>
2020-11-11 17:01:17 +0800

    Add full test cases

--- common_test.go
+++ common_test.go
@@ -3,6 +3,7 @@ package todotxt
 import (
 	"fmt"
 	"strings"
+	"testing"
 )
 
 func compareSlices(list1 []string, list2 []string) bool {
@@ -63,4 +64,62 @@ func strTaskSegmentList(l []*TaskSegment) string {
 		parts = append(parts, fmt.Sprintf("%v", *s))
 	}
 	return strings.Join(parts, ", ")
+}
+
+func Test_lessStrings(t *testing.T) {
+	tests := []struct {
+		a    []string
+		b    []string
+		want bool
+	}{
+		{[]string{"a", "b", "c"}, []string{"a", "b", "c"}, false},
+		{[]string{"a", "b", "c"}, []string{"a", "b"}, false},
+		{[]string{"a", "b", "c"}, []string{"a", "c"}, true},
+		{[]string{"a", "b", "c"}, []string{"b"}, true},
+		{[]string{"a", "b", "c"}, []string{""}, false},
+		{[]string{"a", "b", "c"}, []string{}, false},
+		{[]string{"a", "b"}, []string{"a", "b", "c"}, true},
+		{[]string{"a", "b"}, []string{"a", "a"}, false},
+		{[]string{"a", "b"}, []string{"a", "c"}, true},
+		{[]string{"a", "b"}, []string{"b"}, true},
+		{[]string{"a", "a"}, []string{"a", "b", "c"}, true},
+		{[]string{"a", "a"}, []string{"a", "b"}, true},
+		{[]string{"a", "a"}, []string{"a", "a"}, false},
+		{[]string{"a", "a"}, []string{"a", "c"}, true},
+		{[]string{"a", "a"}, []string{"b"}, true},
+		{[]string{"a", "a"}, []string{"b", "a"}, true},
+		{[]string{"a", "a"}, []string{"c"}, true},
+		{[]string{"a", "c"}, []string{"a", "b", "c"}, false},
+		{[]string{"a", "c"}, []string{"a", "c"}, false},
+		{[]string{"a", "c"}, []string{"b"}, true},
+		{[]string{"a", "c"}, []string{"c"}, true},
+		{[]string{"b"}, []string{"a", "b", "c"}, false},
+		{[]string{"b"}, []string{"a", "c"}, false},
+		{[]string{"b"}, []string{"b"}, false},
+		{[]string{"b"}, []string{"b", "a"}, true},
+		{[]string{"b"}, []string{"c"}, true},
+		{[]string{"b"}, []string{""}, false},
+		{[]string{"b"}, []string{}, false},
+		{[]string{"b", "a"}, []string{"a", "b", "c"}, false},
+		{[]string{"b", "a"}, []string{"b"}, false},
+		{[]string{"b", "a"}, []string{"b", "a"}, false},
+		{[]string{"b", "a"}, []string{"c"}, true},
+		{[]string{"c"}, []string{"a", "b", "c"}, false},
+		{[]string{"c"}, []string{"b"}, false},
+		{[]string{""}, []string{"a", "b", "c"}, true},
+		{[]string{""}, []string{"c"}, true},
+		{[]string{""}, []string{""}, false},
+		{[]string{""}, []string{}, false},
+		{[]string{}, []string{"a", "b", "c"}, true},
+		{[]string{}, []string{"c"}, true},
+		{[]string{}, []string{""}, true},
+		{[]string{}, []string{}, false},
+	}
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("Case#%d", i+1), func(t *testing.T) {
+			if got := lessStrings(tt.a, tt.b); got != tt.want {
+				t.Errorf("lessStrings() %v < %v got = %v, want %v", tt.a, tt.b, got, tt.want)
+			}
+		})
+	}
 }
