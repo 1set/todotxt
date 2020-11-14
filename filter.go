a90a5ece6df33391329aa3d02be19c5d70550a2f
vt128 <jiduduluo@gmail.com>
2020-11-14 17:16:28 +0800

    Add filters

--- filter.go
+++ filter.go
@@ -1,5 +1,7 @@
 package todotxt
 
+import "strings"
+
 // Filter filters the current TaskList for the given predicate (a function that takes a task as input and returns a bool),
 // and returns a new TaskList. The original TaskList is not modified.
 func (tasklist *TaskList) Filter(predicate func(Task) bool) *TaskList {
@@ -15,4 +17,56 @@ func (tasklist *TaskList) Filter(predicate func(Task) bool) *TaskList {
 // FilterCompleted filters completed tasks.
 func FilterCompleted(t Task) bool {
 	return t.Completed
+}
+
+// FilterNotCompleted filters tasks that have not been completed.
+func FilterNotCompleted(t Task) bool {
+	return !t.Completed
+}
+
+// FilterDueToday filters tasks that are due today.
+func FilterDueToday(t Task) bool {
+	return t.IsDueToday()
+}
+
+// FilterOverdue filters tasks that are overdue.
+func FilterOverdue(t Task) bool {
+	return t.IsOverdue()
+}
+
+func FilterHasDueDate(t Task) bool {
+	return t.HasDueDate()
+}
+
+func FilterHasPriority(t Task) bool {
+	return t.HasPriority()
+}
+
+func FilterByPriority(priority string) func(Task) bool {
+	priority = strings.ToUpper(priority)
+	return func(t Task) bool {
+		return t.Priority == priority
+	}
+}
+
+func FilterByProject(project string) func(Task) bool {
+	return func(t Task) bool {
+		for _, p := range t.Projects {
+			if strings.EqualFold(p, project) {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+func FilterByContext(context string) func(Task) bool {
+	return func(t Task) bool {
+		for _, c := range t.Contexts {
+			if strings.EqualFold(c, context) {
+				return true
+			}
+		}
+		return false
+	}
 }
